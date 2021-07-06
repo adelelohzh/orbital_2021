5c43d7706a4a5de33617519eca62afcb8b2f03fe
jetrz <joshuaemmanuelteo@gmail.com>
2021-07-06 15:27:57 +0800

    initdb

--- Header/Header.php
+++ Header/Header.php
@@ -33,7 +33,7 @@
                     <li class = "nostyle inlineblock"><a href='../Main/Main.php' class = "nodeco blockdisplay">Home</a></li>
                     <li class = "nostyle inlineblock"><a href='../Timetable/Timetable.php' class = "nodeco blockdisplay">Timetable</a></li>
                     <li class = "nostyle inlineblock"><a href='../ToDo_List/todolist.php' class = "nodeco blockdisplay">To-Do List</a></li>
-                    <li class = "nostyle inlineblock"><a href='../Shuttle_Bus/shuttlebus.php' class = "nodeco blockdisplay">Shuttle Bus</a></li>
+                    <li class = "nostyle inlineblock"><a href='../Shuttle/Shuttle.php' class = "nodeco blockdisplay">Shuttle Bus</a></li>
                     <li class = "nostyle inlineblock"><a href='../Login_Signup/includes/logout.inc.php' class = "nodeco blockdisplay">Logout</a></li>
                 </ul>
         <?php } ?>       
