31e0b6330a56066c78137bbb5a5342df7d623b85
Adele Loh <adelelohzh@gmail.com>
2021-08-10 15:31:41 +0800

    edited all database-connection.php

--- Timetable/includes/dbh.inc.php
+++ Timetable/includes/dbh.inc.php
@@ -1,10 +1,28 @@
 <?php
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "orbitaldatabase";
+// include_once "initdb.php";
+// include_once "alterdb.php";
 
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+// $serverName = "localhost";
+// $dBUsername = "root";
+// $dBPassword = "";
+// $dBName = "orbitaldatabase";
+
+// $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+
+// if (!$conn) {
+// 	die("Connection Failed: " . mysqli_connect_error());	
+// }
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
